f62e6e704b4666f1b54fa50b8fd1d8fbecc66bec
1D1XZ <adrianestur41n@gmail.com>
2025-07-09 15:40:39 -0500

    Delete src directory

--- src/utils/NiceMessages.js
+++
@@ -1,4 +0,0 @@
-export const NiceMessages = {
-    player_not_in_server: "Parece que no te has unido aun al servidor de minecraft, entra al servidor e intentalo denuevo :D",
-    client_with_gamertag_already_connected: "ya existe alguien usando voicechunk con ese gamertag dentro del servidor. :c"
-}
